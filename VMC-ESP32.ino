c72e3d544cfaabbd38b2af1fb1245a548db3d64a
Ethan <79857871+EthanGely@users.noreply.github.com>
2025-04-15 08:40:28 +0200

    Update VMC-ESP32.ino

--- VMC-ESP32.ino
+++ VMC-ESP32.ino
@@ -4,145 +4,283 @@
 const char* ssid = "Martin Router King";
 const char* password = "I have a LAN";
 
-const int green = 16;
-const int yellow = 17;
-const int red = 18;
+// DTH22 SENSOR //
+//const int DHT22 = 4; // power on 3.3V
+const int DHT22 = 34;  // power on 3.3V
 
-String currentColor = "";
+// FAN RELAYS //
+const int FAN_RELAY = 23;  // Turn FAN ON / OFF
+
+// STEPPER MOTOR //
+const int STEPPER_STEP = 26;  // Moves motor
+const int STEPPER_DIR = 27;   // Set motor direction
+
+// OVERRIDE BUTTON //
+const int OVERRIDE = 33;  // Override button
+
+const int INTERRUPTED = 19;
+
+// variables
+volatile bool buttonPressed = false;
+bool fanActiveOverride = false;
+bool fanActiveAPI = false;
+bool isFanActive = false;
+bool isInterrupted = false;
+
+int temperature = 0;
+int hydrometrie = 0;
+
+unsigned long lastCheckTimeInt = 0;
 
 WebServer server(80);
 
-void handle_On() {
-  if (server.args() == 0) {
-    server.send(400, "text/plain", "Missing color parameter");
+void IRAM_ATTR handleButtonPress() {
+  if (!isInterrupted && millis() - lastCheckTimeInt > 1500) {
+    lastCheckTimeInt = millis();
+    Serial.print("BUTTON PRESS : ");
+    fanActiveOverride = !fanActiveOverride;
+    Serial.println(fanActiveOverride);
+  }
+}
+
+void handle_start() {
+  if (!isInterrupted) {
+    if (!fanActiveOverride) {
+      if (!isFanActive) {
+        fanActiveAPI = true;
+        activateFan();
+        server.send(200, "text/plain", "Fan Active");
+        printInfo();
+        return;
+      } else {
+        server.send(200, "text/plain", "Fan already active");
+        printInfo();
+        return;
+      }
+    } else {
+        server.send(500, "text/plain", "Button is overriding");
+        printInfo();
+        return;
+    }
+  } else {
+    server.send(500, "text/plain", "Already changing state");
+    printInfo();
     return;
   }
+  server.send(404, "text/plain", "ERROR");
+  printInfo();
+}
 
-  String color = server.arg(0);  // Get the first argument (color)
-
-  int pin = getPinFromColor(color);
-  if (pin != -1) {
-    if (pin != green) {
-      digitalWrite(green, LOW);
-    }
-    if (pin != yellow) {
-      digitalWrite(yellow, LOW);
-    }
-    if (pin != red) {
-      digitalWrite(red, LOW);
-    }
-    digitalWrite(pin, HIGH);
-    currentColor = color;
-    server.send(200, "text/plain", color + " ON");
-  } else {
-    if (color == "off") {
-      digitalWrite(green, LOW);
-      digitalWrite(yellow, LOW);
-      digitalWrite(red, LOW);
-      currentColor = "ALL_OFF";
-      server.send(200, "text/plain", "LIGHTS OFF");
-    } else if (color == "allOn") {
-      digitalWrite(green, HIGH);
-      digitalWrite(yellow, HIGH);
-      digitalWrite(red, HIGH);
-      currentColor = "ALL_ON";
-      server.send(200, "text/plain", "LIGHTS ON");
-    } else if (color == "hell") {
-      for (int i = 0; i < 100; i++) {
-        digitalWrite(green, random(2) ? HIGH : LOW);
-        digitalWrite(yellow, random(2) ? HIGH : LOW);
-        digitalWrite(red, random(2) ? HIGH : LOW);
-        delay(50);
+void handle_stop() {
+  if (!isInterrupted) {
+    if (!fanActiveOverride) {
+      if (isFanActive) {
+        fanActiveAPI = false;
+        stopFan();
+        server.send(200, "text/plain", "Fan Stopped");
+        printInfo();
+        return;
+      } else {
+        server.send(200, "text/plain", "Fan already stopped");
+        printInfo();
+        return;
       }
-      currentColor = "HELL";
-      server.send(200, "text/plain", "HELL");
+    } else {
+        server.send(500, "text/plain", "Button is overriding");
+        printInfo();
+        return;
     }
-    server.send(400, "text/plain", "Invalid color");
+  } else {
+    server.send(500, "text/plain", "Already changing state");
+    printInfo();
+    return;
   }
+  server.send(404, "text/plain", "ERROR");
+  printInfo();
 }
 
-void handle_AllOn() {
-  digitalWrite(green, HIGH);
-  digitalWrite(yellow, HIGH);
-  digitalWrite(red, HIGH);
-  currentColor = "ALL_ON";
-  server.send(200, "text/plain", "LIGHTS ON");
-}
+void handle_info() {
+  printInfo();
+  String message = "";
 
-void handle_Home() {
-  server.send(200, "text/plain", currentColor);
-}
+  if (isInterrupted) {
+    if (isFanActive) {
+      message = message + "En train de s'éteindre. ";
+    } else {
+      message = message + "En train de s'allummer. ";
+    }
+  } else {
+    if (isFanActive) {
+      message = message + "Vetilation ON. ";
+    } else {
+      message = message + "Ventialtion OFF. ";
+    }
+  }
 
-int getPinFromColor(String color) {
-  if (color == "green") return green;
-  if (color == "yellow") return yellow;
-  if (color == "red") return red;
-  return -1;  // Invalid color
+  if (fanActiveOverride) {
+    message = message + "Bouton override actif. ";
+  }
+
+  if (fanActiveAPI) {
+    message = message + "Appel API ON. ";
+  }
+
+  server.send(200, "application/json", "{\"message\" : \"" + message + "\",\"status\" : \"" + isFanActive + "\",\"temperature\" : \"" + temperature + "\",\"hygrometrie\" : \"" + hydrometrie + "\"}");
 }
 
 void setup() {
   Serial.begin(115200);
-  pinMode(green, OUTPUT);
-  pinMode(yellow, OUTPUT);
-  pinMode(red, OUTPUT);
-  
+  // DTH22 sensor
+  pinMode(DHT22, INPUT);
+
+  // FAN RELAY
+  pinMode(FAN_RELAY, OUTPUT);
+
+  // STEPPER
+  pinMode(STEPPER_STEP, OUTPUT);
+  pinMode(STEPPER_DIR, OUTPUT);
+
+  // OVERRIDE BUTTON
+  pinMode(OVERRIDE, INPUT_PULLUP);
+
+  //INTEERUPTION LED
+  pinMode(INTERRUPTED, OUTPUT);
+
   WiFi.begin(ssid, password);
   Serial.print("Connecting to Wi-Fi");
-  
+
   while (WiFi.status() != WL_CONNECTED) {
-    load();
     Serial.print(".");
   }
-  
-  digitalWrite(yellow, LOW);
+
   Serial.println("\nConnected to Wi-Fi!");
   Serial.print("IP Address: ");
   Serial.println(WiFi.localIP());
 
-  server.on("/", handle_Home);
-  server.on("/on", handle_On);
-  
   server.begin();
-  startupColors();
+  server.on("/start-vmc", handle_start);
+  server.on("/stop-vmc", handle_stop);
+  server.on("/info", handle_info);
 
+  attachInterrupt(digitalPinToInterrupt(OVERRIDE), handleButtonPress, FALLING);
+
+
+  Serial.println("----BEGIN----");
+  digitalWrite(INTERRUPTED, HIGH);
+  digitalWrite(FAN_RELAY, HIGH);
+  digitalWrite(STEPPER_DIR, HIGH);
+  digitalWrite(STEPPER_STEP, HIGH);
+  delay(4000);
+  digitalWrite(INTERRUPTED, LOW);
+  digitalWrite(FAN_RELAY, LOW);
+  digitalWrite(STEPPER_DIR, LOW);
+  digitalWrite(STEPPER_STEP, LOW);
 }
 
 void loop() {
+  static unsigned long lastCheckTime = 0;
   server.handleClient();
+
+  if (fanActiveOverride) {
+    if (!isFanActive) {
+      Serial.println("Override button pressed!");
+      activateFan();
+      printInfo();
+    }
+    return;
+  }
+
+  if (millis() - lastCheckTime > 1500) {
+    lastCheckTime = millis();
+    int lightValue = analogRead(DHT22);
+    lightValue = 4095 - lightValue;
+
+    temperature = lightValue;
+    hydrometrie = lightValue;
+
+    
+    if (fanActiveAPI) {
+      if (!isFanActive) {
+        Serial.println("Activating fan from API");
+        activateFan();
+        printInfo();
+      }
+      return;
+    } else {
+      if (lightValue < 2000) {
+        if (!isFanActive) {
+          Serial.println("Activating fan from SENSOR");
+          activateFan();
+          printInfo();
+        }
+        return;
+      } else if (isFanActive) {
+        stopFan();
+        printInfo();
+      }
+    }
+  }
 }
 
-void startupColors() {
-  digitalWrite(green, HIGH);
-  delay(300);
-  digitalWrite(green, LOW);
-  digitalWrite(yellow, HIGH);
-  delay(300);
-  digitalWrite(yellow, LOW);
-  digitalWrite(red, HIGH);
-  delay(300);
-  digitalWrite(red, LOW);
-  delay(600);
-  digitalWrite(green, HIGH);
-  digitalWrite(yellow, HIGH);
-  digitalWrite(red, HIGH);
-  delay(200);
-  digitalWrite(green, LOW);
-  digitalWrite(yellow, LOW);
-  digitalWrite(red, LOW);
+void activateFan() {
+  isInterrupted = true;
+  digitalWrite(INTERRUPTED, HIGH);
+  Serial.println("Opening gate...");
+  digitalWrite(STEPPER_DIR, HIGH);
+  digitalWrite(STEPPER_STEP, HIGH);
+  delay(4000);
+  Serial.println("Gate open !");
+  digitalWrite(STEPPER_DIR, LOW);
+  digitalWrite(STEPPER_STEP, LOW);
+  digitalWrite(FAN_RELAY, HIGH);
+  isFanActive = true;
+  Serial.println("Fan ON");
+  digitalWrite(INTERRUPTED, LOW);
+  isInterrupted = false;
 }
 
+void stopFan() {
+  isInterrupted = true;
+  digitalWrite(INTERRUPTED, HIGH);
+  Serial.println("FAN OFF");
+  digitalWrite(FAN_RELAY, LOW);
+  Serial.println("Waiting for fan to slow down");
+  delay(2000);
+  Serial.println("Closing gate...");
+  digitalWrite(STEPPER_DIR, LOW);
+  digitalWrite(STEPPER_STEP, HIGH);
+  delay(4000);
+  digitalWrite(STEPPER_STEP, LOW);
+  Serial.println("Gate closed !");
+  isFanActive = false;
+  digitalWrite(INTERRUPTED, LOW);
+  isInterrupted = false;
+}
 
-void load() {
-  digitalWrite(green, HIGH);
-  delay(200);
-  digitalWrite(yellow, HIGH);
-  delay(50);
-  digitalWrite(green, LOW);
-  delay(150);
-  digitalWrite(red, HIGH);
-  delay(50);
-  digitalWrite(yellow, LOW);
-  delay(150);
-  digitalWrite(red, LOW);
-  delay(50);
+void printInfo() {
+  Serial.println("---------- IMFORMATION DUMP ----------");
+  Serial.print("Fan status : ");
+  if (isFanActive) {
+    Serial.println("1");
+  } else {
+    Serial.println("0");
+  }
+  Serial.print("Interruption : ");
+  if (isInterrupted) {
+    Serial.println("1");
+  } else {
+    Serial.println("0");
+  }
+  Serial.print("Override active : ");
+  if (fanActiveOverride) {
+    Serial.println("1");
+  } else {
+    Serial.println("0");
+  }
+  Serial.print("API active : ");
+  if (fanActiveAPI) {
+    Serial.println("1");
+  } else {
+    Serial.println("0");
+  }
 }
