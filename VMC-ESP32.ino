4352b64acedfe5b53c37bae1f4dafc6b4cc08e06
Ethan <79857871+EthanGely@users.noreply.github.com>
2025-04-16 16:52:50 +0200

    Update VMC-ESP32.ino

--- VMC-ESP32.ino
+++ VMC-ESP32.ino
@@ -1,24 +1,43 @@
 #include <WiFi.h>
 #include <WebServer.h>
+#include "DHT.h"
 
 const char* ssid = "Martin Router King";
 const char* password = "I have a LAN";
 
+const int temperatureTreshold = 35;
+const int hygrometryTreshold = 70;
+
 // DTH22 SENSOR //
-//const int DHT22 = 4; // power on 3.3V
-const int DHT22 = 34;  // power on 3.3V
+#define DHTPIN 5       // GPIO pin you're using
+#define DHTTYPE DHT22   // DHT22 (AM2302)
+
+DHT dht(DHTPIN, DHTTYPE);
 
 // FAN RELAYS //
-const int FAN_RELAY = 23;  // Turn FAN ON / OFF
+const int FAN_RELAY = 4;  // Turn FAN ON / OFF
 
 // STEPPER MOTOR //
-const int STEPPER_STEP = 26;  // Moves motor
-const int STEPPER_DIR = 27;   // Set motor direction
+#define IN1 17
+#define IN2 18
+#define IN3 7
+#define IN4 16
+
+const int steps[8][4] = {
+  {1, 0, 0, 0},
+  {1, 1, 0, 0},
+  {0, 1, 0, 0},
+  {0, 1, 1, 0},
+  {0, 0, 1, 0},
+  {0, 0, 1, 1},
+  {0, 0, 0, 1},
+  {1, 0, 0, 1}
+};
 
 // OVERRIDE BUTTON //
-const int OVERRIDE = 33;  // Override button
+//const int OVERRIDE = 15;  // Override button
 
-const int INTERRUPTED = 19;
+const int INTERRUPTED = 40;
 
 // variables
 volatile bool buttonPressed = false;
@@ -34,22 +53,36 @@ unsigned long lastCheckTimeInt = 0;
 
 WebServer server(80);
 
+/*
 void IRAM_ATTR handleButtonPress() {
-  if (!isInterrupted && millis() - lastCheckTimeInt > 1500) {
+
+    if (!isInterrupted && millis() - lastCheckTimeInt > 1500) {
     lastCheckTimeInt = millis();
     Serial.print("BUTTON PRESS : ");
     fanActiveOverride = !fanActiveOverride;
     Serial.println(fanActiveOverride);
   }
-}
+}*/
+
 
 void handle_start() {
+  digitalWrite(INTERRUPTED, HIGH);
+  delay(500);
+  digitalWrite(INTERRUPTED, LOW);
+  delay(100);
+  digitalWrite(INTERRUPTED, HIGH);
+  delay(100);
+  digitalWrite(INTERRUPTED, LOW);
+  delay(100);
+  digitalWrite(INTERRUPTED, HIGH);
+  delay(500);
+  digitalWrite(INTERRUPTED, LOW);
   if (!isInterrupted) {
     if (!fanActiveOverride) {
       if (!isFanActive) {
         fanActiveAPI = true;
-        activateFan();
         server.send(200, "text/plain", "Fan Active");
+        activateFan();
         printInfo();
         return;
       } else {
@@ -72,12 +105,23 @@ void handle_start() {
 }
 
 void handle_stop() {
+  digitalWrite(INTERRUPTED, HIGH);
+  delay(500);
+  digitalWrite(INTERRUPTED, LOW);
+  delay(100);
+  digitalWrite(INTERRUPTED, HIGH);
+  delay(100);
+  digitalWrite(INTERRUPTED, LOW);
+  delay(100);
+  digitalWrite(INTERRUPTED, HIGH);
+  delay(500);
+  digitalWrite(INTERRUPTED, LOW);
   if (!isInterrupted) {
     if (!fanActiveOverride) {
       if (isFanActive) {
         fanActiveAPI = false;
-        stopFan();
         server.send(200, "text/plain", "Fan Stopped");
+        stopFan(true);
         printInfo();
         return;
       } else {
@@ -130,29 +174,34 @@ void handle_info() {
 
 void setup() {
   Serial.begin(115200);
-  // DTH22 sensor
-  pinMode(DHT22, INPUT);
+  dht.begin();
 
   // FAN RELAY
   pinMode(FAN_RELAY, OUTPUT);
 
-  // STEPPER
-  pinMode(STEPPER_STEP, OUTPUT);
-  pinMode(STEPPER_DIR, OUTPUT);
-
   // OVERRIDE BUTTON
-  pinMode(OVERRIDE, INPUT_PULLUP);
+  //pinMode(OVERRIDE, INPUT_PULLUP); 
 
   //INTEERUPTION LED
   pinMode(INTERRUPTED, OUTPUT);
+
+  pinMode(IN1, OUTPUT);
+  pinMode(IN2, OUTPUT);
+  pinMode(IN3, OUTPUT);
+  pinMode(IN4, OUTPUT);
+
+  stopFan(false);
 
   WiFi.begin(ssid, password);
   Serial.print("Connecting to Wi-Fi");
 
   while (WiFi.status() != WL_CONNECTED) {
     Serial.print(".");
+    digitalWrite(INTERRUPTED, HIGH);
+    delay(150);
+    digitalWrite(INTERRUPTED, LOW);
   }
-
+  digitalWrite(INTERRUPTED, LOW);
   Serial.println("\nConnected to Wi-Fi!");
   Serial.print("IP Address: ");
   Serial.println(WiFi.localIP());
@@ -162,19 +211,10 @@ void setup() {
   server.on("/stop-vmc", handle_stop);
   server.on("/info", handle_info);
 
-  attachInterrupt(digitalPinToInterrupt(OVERRIDE), handleButtonPress, FALLING);
+  //attachInterrupt(digitalPinToInterrupt(OVERRIDE), handleButtonPress, FALLING);
 
 
   Serial.println("----BEGIN----");
-  digitalWrite(INTERRUPTED, HIGH);
-  digitalWrite(FAN_RELAY, HIGH);
-  digitalWrite(STEPPER_DIR, HIGH);
-  digitalWrite(STEPPER_STEP, HIGH);
-  delay(4000);
-  digitalWrite(INTERRUPTED, LOW);
-  digitalWrite(FAN_RELAY, LOW);
-  digitalWrite(STEPPER_DIR, LOW);
-  digitalWrite(STEPPER_STEP, LOW);
 }
 
 void loop() {
@@ -192,12 +232,9 @@ void loop() {
 
   if (millis() - lastCheckTime > 1500) {
     lastCheckTime = millis();
-    int lightValue = analogRead(DHT22);
-    lightValue = 4095 - lightValue;
 
-    temperature = lightValue;
-    hydrometrie = lightValue;
-
+  hydrometrie = dht.readHumidity();
+  temperature = dht.readTemperature();
     
     if (fanActiveAPI) {
       if (!isFanActive) {
@@ -207,7 +244,7 @@ void loop() {
       }
       return;
     } else {
-      if (lightValue < 2000) {
+      if (temperature >= temperatureTreshold || hydrometrie >= hygrometryTreshold) {
         if (!isFanActive) {
           Serial.println("Activating fan from SENSOR");
           activateFan();
@@ -215,7 +252,7 @@ void loop() {
         }
         return;
       } else if (isFanActive) {
-        stopFan();
+        stopFan(true);
         printInfo();
       }
     }
@@ -223,64 +260,81 @@ void loop() {
 }
 
 void activateFan() {
+  Serial.println("---START---");
   isInterrupted = true;
   digitalWrite(INTERRUPTED, HIGH);
-  Serial.println("Opening gate...");
-  digitalWrite(STEPPER_DIR, HIGH);
-  digitalWrite(STEPPER_STEP, HIGH);
-  delay(4000);
-  Serial.println("Gate open !");
-  digitalWrite(STEPPER_DIR, LOW);
-  digitalWrite(STEPPER_STEP, LOW);
   digitalWrite(FAN_RELAY, HIGH);
   isFanActive = true;
-  Serial.println("Fan ON");
+  turnStepper(120, true);
   digitalWrite(INTERRUPTED, LOW);
   isInterrupted = false;
 }
 
-void stopFan() {
+void stopFan(bool gate) {
+   Serial.println("---STOP---");
   isInterrupted = true;
   digitalWrite(INTERRUPTED, HIGH);
-  Serial.println("FAN OFF");
   digitalWrite(FAN_RELAY, LOW);
-  Serial.println("Waiting for fan to slow down");
-  delay(2000);
-  Serial.println("Closing gate...");
-  digitalWrite(STEPPER_DIR, LOW);
-  digitalWrite(STEPPER_STEP, HIGH);
-  delay(4000);
-  digitalWrite(STEPPER_STEP, LOW);
-  Serial.println("Gate closed !");
+
+  if(gate) {
+    delay(2000);
+    turnStepper(120, false);
+  }
+  
   isFanActive = false;
   digitalWrite(INTERRUPTED, LOW);
   isInterrupted = false;
 }
 
 void printInfo() {
-  Serial.println("---------- IMFORMATION DUMP ----------");
-  Serial.print("Fan status : ");
+  //Serial.println("---------- IMFORMATION DUMP ----------");
+  //Serial.print("Fan status : ");
   if (isFanActive) {
-    Serial.println("1");
+    //Serial.println("1");
   } else {
-    Serial.println("0");
+    //Serial.println("0");
   }
-  Serial.print("Interruption : ");
+  //Serial.print("Interruption : ");
   if (isInterrupted) {
-    Serial.println("1");
+    //Serial.println("1");
   } else {
-    Serial.println("0");
+    //Serial.println("0");
   }
-  Serial.print("Override active : ");
+  //Serial.print("Override active : ");
   if (fanActiveOverride) {
-    Serial.println("1");
+    //Serial.println("1");
   } else {
-    Serial.println("0");
+    //Serial.println("0");
   }
-  Serial.print("API active : ");
+  //Serial.print("API active : ");
   if (fanActiveAPI) {
-    Serial.println("1");
+    //Serial.println("1");
   } else {
-    Serial.println("0");
+    //Serial.println("0");
   }
+}
+
+void turnStepper(int deg, bool sens) {
+  int degCorr = round(deg * 11.32);
+  for (int i = 0; i < degCorr; i++) {  // 512 steps for a full rotation
+    if (sens) {
+      stepMotor(i % 8);
+      delay(3);
+    } else {
+      int stepIndex = (8 - (i % 8)) % 8;
+      stepMotor(stepIndex);
+      delay(3);
+    }
+  }
+  digitalWrite(IN1, 0);
+  digitalWrite(IN2, 0);
+  digitalWrite(IN3, 0);
+  digitalWrite(IN4, 0);
+}
+
+void stepMotor(int step) {
+  digitalWrite(IN1, steps[step][0]);
+  digitalWrite(IN2, steps[step][1]);
+  digitalWrite(IN3, steps[step][2]);
+  digitalWrite(IN4, steps[step][3]);
 }
